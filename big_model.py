770a0574519f4cfa7525208f6ba629e61548ea45
Alexis Ross <alexisjihyeross@gmail.com>
2020-08-04 00:23:20 -0500

    make sure to remember that adult and bail experiments were with small model

---
+++ big_model.py
@@ -0,0 +1,19 @@
+import torch
+
+# define model class
+class BigModel(torch.nn.Module):
+
+    def __init__(self, input_size, hidden_size = 500):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(input_size, hidden_size)
+        self.linear2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.linear3 = torch.nn.Linear(hidden_size, 1)
+        self.drop = torch.nn.Dropout(p=0.3)
+        self.activ = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.drop(self.activ(self.linear1(x)))
+        y = self.drop(self.activ(self.linear2(y)))
+        y = (self.linear3(y))
+        return self.sigmoid(y)
