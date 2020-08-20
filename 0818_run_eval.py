ae3d64a492c55dba678450ac8de65cf49da4273e
Alexis Ross <alexisjihyeross>
2020-08-19 22:47:00 -0400

    bug fix

--- 0818_run_eval.py
+++ 0818_run_eval.py
@@ -12,11 +12,15 @@ compas_X, compas_y, compas_actionable_indices, compas_categorical_features, comp
 compas_data = read_data(compas_experiment_dir)
 
 delta_max = 0.75
-weight_dir = compas_experiment_dir + str(w) + "/"
+
+data = compas_data
+experiment_dir = compas_experiment_dir
+actionable_indices = compas_actionable_indices
 
 for w in weights_to_eval:
     print("WEIGHT: ", w)    
-    model = load_torch_model(weight_dir, weight)
+    weight_dir = experiment_dir + str(w) + "/"
+    model = load_torch_model(weight_dir, w)
 
 	# Runs wachter + our evaluation for every threshold in the 'WEIGHT_val_thresholds_info.csv' file output by the train function
-    run_evaluate(model, data, w, delta_max, actionable_indices, compas_experiment_dir, lam_init = 0.005, data_indices = range(0, 250), thresholds = thresholds_to_eval)
+    run_evaluate(model, data, w, delta_max, actionable_indices, experiment_dir, lam_init = 0.005, data_indices = range(0, 250), thresholds = thresholds_to_eval)
