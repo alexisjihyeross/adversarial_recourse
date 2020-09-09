9a31edd39aadfb1575f1e39d51603f2857ce104f
Alexis Ross <alexisjihyeross@gmail.com>
2020-09-09 16:28:39 -0400

    changes

--- bail.py
+++ bail.py
@@ -7,12 +7,12 @@ from utils import *
 bail_X, bail_y, bail_actionable_indices, bail_categorical_features, bail_categorical_names = process_bail_data()
 bail_experiment_dir = 'new_results/0907_bail_0.5/'
 
-bail_data = get_data(bail_X, bail_y)
-write_data(bail_data, bail_experiment_dir)
+# bail_data = get_data(bail_X, bail_y)
+# write_data(bail_data, bail_experiment_dir)
 
-# bail_data = read_data(bail_experiment_dir)
+bail_data = read_data(bail_experiment_dir)
 
 delta_max = 0.5
 thresholds_to_eval = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
 
-run(bail_data, bail_actionable_indices, bail_categorical_features, bail_experiment_dir, [0.0, 0.05, 0.1, 0.25, 0.5], delta_max, do_train = True, thresholds_to_eval = thresholds_to_eval)
+run(bail_data, bail_actionable_indices, bail_categorical_features, bail_experiment_dir, [0.15, 0.2, 0.3, 0.35, 0.4, 0.45], delta_max, do_train = True, thresholds_to_eval = thresholds_to_eval)
