46e4b6981b5507514999812b85d2d8cfa57c3bed
Alexis Ross <alexisjihyeross@gmail.com>
2020-09-09 16:41:01 -0400

    changes

--- bail.py
+++ bail.py
@@ -15,5 +15,5 @@ bail_data = read_data(bail_experiment_dir)
 delta_max = 0.5
 thresholds_to_eval = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
 
-weights = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+weights = [0.15, 0.2, 0.3, 0.35, 0.4, 0.45]
 run(bail_data, bail_actionable_indices, bail_categorical_features, bail_experiment_dir, weights, delta_max, do_train = True, thresholds_to_eval = thresholds_to_eval)
