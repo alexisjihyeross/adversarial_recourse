03729d2347d6b341c9a38eb225e6ecbdf262583f
Alexis Ross <alexisjihyeross@gmail.com>
2020-09-09 23:47:04 -0400

    changes

--- compas.py
+++ compas.py
@@ -11,7 +11,7 @@ compas_data = read_data(compas_experiment_dir)
 # compas_data = get_data(compas_X, compas_y)
 # write_data(compas_data, compas_experiment_dir)
 
-delta_max = 0.5
+delta_max = 0.75
 thresholds_to_eval = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
 
 weights = [0.15, 0.2, 0.3, 0.35, 0.4, 0.45]
