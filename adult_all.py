da3c87d083a5f1fce66733f90d21845e68ad482d
Alexis Ross <alexisjihyeross>
2020-09-11 13:55:18 -0400

    fix

--- adult_all.py
+++ adult_all.py
@@ -9,11 +9,13 @@ delta_max = 0.75
 adult_X, adult_y, adult_actionable_indices, adult_categorical_features, adult_categorical_names = process_adult_data()
 adult_experiment_dir = 'new_results/0910_experiments/0910_adult_' + str(delta_max) + '/'
 
-adult_data = get_data(adult_X, adult_y)
-write_data(adult_data, adult_experiment_dir)
+#adult_data = get_data(adult_X, adult_y)
+#write_data(adult_data, adult_experiment_dir)
+
+adult_data = read_data(adult_experiment_dir)
 
 weights = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
-run(adult_data, adult_actionable_indices, adult_categorical_features, adult_experiment_dir, weights, delta_max, do_train = True)
+# run(adult_data, adult_actionable_indices, adult_categorical_features, adult_experiment_dir, weights, delta_max, do_train = True)
 
 data = adult_data
 experiment_dir = adult_experiment_dir
