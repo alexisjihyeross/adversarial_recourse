8a5138871ad594d6561eb7596498b689c21ba9d5
Alexis Ross <alexisjihyeross@gmail.com>
2020-10-21 22:45:07 -0400

    test change

--- utils/nlp_utils.py
+++ utils/nlp_utils.py
@@ -68,7 +68,7 @@ def get_sst_data(file_path):
                 sentiment = 1.0
             texts.append(text)
             labels.append(sentiment)
-    return texts[0:10], labels[0:10]
+    return texts[0:200], labels[0:200]
 
 def antonyms(term):
     # helper function that retrieves antonyms for a given term; called in get_candidates
